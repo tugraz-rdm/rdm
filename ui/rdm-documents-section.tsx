3808e7a72b7871fbf4c0ba2ffc43bb37300c8e83
Laura <lth@protechnology.de>
2024-03-28 12:08:45 +0100

    Update CyVerse support email and user portal URLs

--- ui/rdm-documents-section.tsx
+++ ui/rdm-documents-section.tsx
@@ -8,12 +8,15 @@ export type Documents = {
 
 interface RdmDocumentSectionProps {
   isOpen: boolean;
-  documents: Documents[]; 
+  documents: Documents[];
 }
 
-const RdmDocumentSection: React.FC<RdmDocumentSectionProps> = ({ isOpen, documents }) => {
+const RdmDocumentSection: React.FC<RdmDocumentSectionProps> = ({
+  isOpen,
+  documents,
+}) => {
   const handleDownload = (url: string) => {
-    window.location.href = url; 
+    window.location.href = url;
   };
 
   return (
@@ -23,21 +26,25 @@ const RdmDocumentSection: React.FC<RdmDocumentSectionProps> = ({ isOpen, documen
           {documents.map((document, docIndex) => (
             <div key={docIndex}>
               {document.type === 'download' ? (
-                <button
-                  type="button"
-                  className="hover:bg-white-smoke group flex w-full items-center rounded-lg p-2 text-base hover:text-gray-700 text-white-smoke transition duration-75 dark:text-white-smoke dark:hover:bg-white-smoke"
-                  onClick={() => handleDownload(document.url)}
-                >
-                  <span className="flex-1 whitespace-nowrap text-left font-thin">{document.name}</span>
-                </button>
-              ) : (
                 <a
                   href={document.url}
                   className="hover:bg-white-smoke group flex w-full items-center rounded-lg p-2 text-base hover:text-gray-700 text-white-smoke transition duration-75 dark:text-white-smoke dark:hover:bg-white-smoke"
                   target="_blank"
                   rel="noopener noreferrer"
-                >
-                  <span className="flex-1 whitespace-nowrap text-left font-thin">{document.name}</span>
+                  download>
+                  <span className="flex-1 whitespace-nowrap text-left font-thin">
+                    {document.name}
+                  </span>
+                </a>
+              ) : (
+                <a
+                  href={document.url}
+                  className="hover:bg-white-smoke group flex w-full items-center rounded-lg p-2 text-base hover:text-gray-700 text-white-smoke transition duration-75 dark:text-white-smoke dark:hover:bg-white-smoke"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <span className="flex-1 whitespace-nowrap text-left font-thin">
+                    {document.name}
+                  </span>
                 </a>
               )}
             </div>
