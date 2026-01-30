41cf42a58c0fb6dd396ef47bdbb2db26627cfd5a
Laura <lthaci@tugraz.at>
2026-01-30 12:28:28 +0100

    tmp: hide privat policy until ready

--- lib/documents.ts
+++ lib/documents.ts
@@ -1,11 +1,12 @@
 import { Documents } from '#/ui/rdm-documents-section';
 
 export const documents: Documents[] = [
-  {
-    name: 'PRIVACY POLICY',
-    type: 'download',
-    url: 'https://repository.tugraz.at/static/documents/TUGraz_Repository_General_Data_Protection_Rights_en.pdf',
-  },
+  // TODO: add back when the correct Privacy Policy is ready
+  // {
+  //   name: 'PRIVACY POLICY',
+  //   type: 'download',
+  //   url: 'https://repository.tugraz.at/static/documents/TUGraz_Repository_General_Data_Protection_Rights_en.pdf',
+  // },
   {
     name: 'LEGAL NOTICE',
     type: 'link',
